2052dc4d503195e655a948f1bbd28df1e11fe8d4
Ben <benjamin.schwab.nonspam@gmail.com>
2019-04-11 13:16:48 -0700

    Add purchase model

---
+++ PlattProject/PlattProject/Models/Purchase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlattProject.Models
+{
+    public class Purchase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Item")]
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int NumberPurchased { get; set; }
+        public DateTime PurchaseDate { get; set; }
+
+
+    }
+
+    
+}
